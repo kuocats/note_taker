214aaf637e7d48114630139609acfafd5f2930dc
Vivian Kuo <vivian.kuo4@outlook.com>
2023-08-05 10:31:31 -1000

    updated html.js

---
+++ Develop/public/routes/html.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+const path = require('path');
+
+router.get("/", (req, res) => {
+    res.sendFile(path.join(_dirname, '/../routes/public/index.html'));
+});
+
+router.get("/notes", (req, res) => {
+    res.sendFile(path.join(_direname, '/../public/notes.html'));
+});
+
+module.exports = router;
