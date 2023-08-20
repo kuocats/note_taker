f7fb0b1f0e37fc2823b873d6d681da3fe00fc458
Vivian Kuo <vivian.kuo4@outlook.com>
2023-08-19 14:54:56 -1000

    edit file

--- routes/html.js
+++ routes/html.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const path = require('path');
+var __direname = path.resolve();
 
 router.get("/", (req, res) => {
-    res.sendFile(path.join(_dirname, '../public/index.html'));
+    res.sendFile(path.join(__dirname, '/../routes/public/index.html'));
 });
 
 router.get("/notes", (req, res) => {
-    res.sendFile(path.join(_direname, '../public/notes.html'));
+    res.sendFile(path.join(__dirname, '/../public/notes.html'));
 });
 
 module.exports = router;
