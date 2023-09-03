58f3b9fed053b12cc67ff95e98e83ba37e880d4d
Vivian Kuo <vivian.kuo4@outlook.com>
2023-09-03 10:18:00 -1000

    added uuid.js to help

---
+++ helpers/uuid.js
@@ -0,0 +1,5 @@
+// direct export of function that generates a string of random numbers and letters
+module.exports = () =>
+  Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1);
